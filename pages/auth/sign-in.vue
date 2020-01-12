5646354ae808208c396b53369615b28bf18990e4
Danylo <jerrylite219@gmail.com>
2020-01-12 15:28:44 +0300

    Sing-in template

--- pages/auth/sign-in.vue
+++ pages/auth/sign-in.vue
@@ -1,10 +1,61 @@
 <template>
-  <div>
-    <input type="email" />
-    <input type="text" />
-  </div>
+  <section>
+    <div>
+      <div class="container">
+        <div class="account"></div>
+        <label>Ведіть ваш логін</label>
+        <input type="email" placeholder="email або номер телефону" />
+        <label>Ведіть свій пароль</label>
+        <input type="password" placeholder="Не менше 6 знаків" />
+        <div class="btn">
+          <nuxt-link to="/">Увійти</nuxt-link>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
+<script>
+import AppNavigation from "~/components/main/AppNavigation";
+export default {
+  components: {
+    AppNavigation
+  }
+};
+</script>
 
 <style scoped>
-
+div {
+  display: flex;
+  justify-content: center;
+}
+.account {
+  width: 100px;
+  height: 100px;
+  border-radius: 100%;
+  background-color: #fff;
+  margin-bottom: 20px;
+}
+.container {
+  width: 350px;
+  height: 450px;
+  background-color: grey;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  margin-top: 100px;
+  border-radius: 10px;
+}
+input {
+  margin-bottom: 10px;
+}
+a {
+  text-decoration: none;
+}
+.btn {
+  width: 100px;
+  height: 50px;
+  background-color: #f1f1f1;
+  border-radius: 10px;
+}
 </style>
