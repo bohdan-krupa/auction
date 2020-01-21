f3fec6e2ebd50449cbef8750b6bf3485d35b0a62
Bodya <bodya.save.dev@gmail.com>
2020-01-21 23:20:01 +0200

    merge

--- pages/post/_id.vue
+++ pages/post/_id.vue
@@ -1,38 +1,45 @@
 <template>
-  <div class="main-page">
-    <div class="product">
-      <div class="slider-img">
-        <div class="main-img">
-          <img src="img/mac-1.png" alt="main" />
-        </div>
-        <div class="other-img">
-          <img src="img/mac-2.png" alt="photo" />
-          <img src="img/mac-2.png" alt="photo" />
-          <img src="img/mac-1.png" alt="photo" />
-        </div>
+  <div class="product">
+    <div class="slider-img">
+      <div class="main-img">
+        <img src="img/mac-1.png" alt="main" />
       </div>
-      <div class="item-data">
-        <h3>MacBook Pro</h3>
-        <i>#312311</i>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur recusandae aperiam magnam reprehenderit, aut nam amet ipsa voluptatem eius quam non molestias consequatur, hic velit, officia perferendis! Ea, consequuntur laudantium?</p>
-        <span>Price: 100 грн</span>
-        <div class="btn">Підняти ставку</div>
+      <div class="other-img">
+        <img src="img/mac-2.png" alt="photo" />
+        <img src="img/mac-2.png" alt="photo" />
+        <img src="img/mac-1.png" alt="photo" />
       </div>
+    </div>
+    <div class="item-data">
+      <h3>MacBook Pro</h3>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur recusandae aperiam magnam reprehenderit, aut nam amet ipsa voluptatem eius quam non molestias consequatur, hic velit, officia perferendis! Ea, consequuntur laudantium?</p>
+      <span>
+        Time:
+        <br />12h 13m 42s
+      </span>
+      <span>Price: 100 грн</span>
+      <div class="btn">Підняти ставку</div>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  validate({ params }) {
+    return !isNaN(params.id);
+  }
+};
+</script>
 
 <style lang="sass" scoped>
 @import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro&display=swap')
 
 
-.main-page
-  height: 100vh
-  background: #999
-  margin-top: -100px
 .product
   display: flex
+  flex-wrap: wrap
   padding-top: 180px
+  height: 100vh
 
   .main-img
     margin: 0px 50px 0px 100px
@@ -59,7 +66,7 @@
       font-size: 30px
       font-weight: bold
       color: #d9ff7d
-      margin-top: 50px
+      margin-top: 10px
     
     .btn
       display: flex
